4998ef24c19c6a59ce3fe60df1e83945b53a9af8
frank.sun <elitegoblinrb@gmail.com>
2025-12-14 10:46:57 +1100

    Add ts-context-commentstring for JSX/TSX context-aware comments

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -144,6 +144,17 @@ require("lazy").setup({
     end,
   },
 
+  -- Context-aware comments (for JSX/TSX)
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false, -- We'll integrate with Comment.nvim
+      })
+    end,
+  },
+
   -- LSP Configuration
   {
     "neovim/nvim-lspconfig",
@@ -270,8 +281,11 @@ require("lazy").setup({
   -- Comment plugin
   {
     "numToStr/Comment.nvim",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
     end,
   },
 
